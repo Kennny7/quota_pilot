ad5341f2ad5ab581d59af33ec8681c87ec019635
Kennny7 <khushalpareta9@gmail.com>
2026-09-18 17:02:58 +0530

    refactor: simplify notification providers by exporting from presentation layer

--- lib/core/notifications/notification_providers.dart
+++ lib/core/notifications/notification_providers.dart
@@ -1,28 +1,3 @@
-// lib/presentation/providers/notification_providers.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// lib/core/notifications/notification_providers.dart
 
-import '../../core/notifications/notification_service.dart';
-import '../../core/notifications/quota_alert_scheduler.dart';
-import '../../core/notifications/quota_alert_service.dart';
-import 'settings_providers.dart'; // settingsRepositoryProvider
-
-final notificationServiceProvider = Provider<NotificationService>(
-  (ref) => NotificationService.instance,
-);
-
-final quotaAlertServiceProvider = Provider<QuotaAlertService>((ref) {
-  return QuotaAlertService(
-    accountRepository: ref.watch(accountRepositoryProvider),
-    quotaRepository: ref.watch(quotaRepositoryProvider),
-    settingsRepository: ref.watch(settingsRepositoryProvider),
-    notifications: ref.watch(notificationServiceProvider),
-  );
-});
-
-/// Watch this once (e.g. in the root widget) to start the start/resume checks.
-final quotaAlertSchedulerProvider = Provider<QuotaAlertScheduler>((ref) {
-  final scheduler = QuotaAlertScheduler(ref.watch(quotaAlertServiceProvider));
-  scheduler.start();
-  ref.onDispose(scheduler.stop);
-  return scheduler;
-});
+export '../../presentation/providers/notification_providers.dart';
